7bfcce7fda6c1050ee3733fbe89768c60ab5916c
urana123 <uranhand60@gmail.com>
2022-11-05 15:04:57 +0800

    marat

--- src/pages/admin/AdminContainer.js
+++ src/pages/admin/AdminContainer.js
@@ -3,7 +3,13 @@ import { Outlet } from 'react-router-dom'
 export const AdminContainer = () => {
 
     return (
-        <div className='w-screen h-screen bg-green-200'>
+        <div className='w-screen h-screen bg-green-200 '>
+
+            <div ></div>
+
+            <div className='flex'>
+
+            </div>
             <Outlet />
         </div>
     )
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        //common
+        "light": "#FFFFFF",
+        "light-40": "rgb(255,255,255, 0.4)",
+        "darken": "#1E1F21",
+        "dark-grey": "#2E2E30",
+        "purple": "#9264F3",
+        "purple-secondary": "rgba(146, 100, 243, 0.1)",
+        "grey": "#696969",
+        "disabled": "#A19FA1",
+        "border": "#424244",
+      },
+    },
   },
   plugins: [],
 }
